e6df762f1346bb104180a881974b491b898bddcd
Findlay Bewicke-copley <f.copley@qmul.ac.uk>
2018-10-19 10:46:18 +0100

    Update Fastqc_array.sh

--- Fastqc_array.sh
+++ Fastqc_array.sh
@@ -40,9 +40,12 @@ while [ "$1" != "" ]; do
         shift
 done
 
+echo MODE set to $MODE
 if [[ $MODE -eq fastq ]]; then
+	echo 'will set FILEDIR to FASTQ_Raw/*/*' 
 	FILEDIR=FASTQ_Raw/*/*
 elif [[ $MODE -eq bam ]]; then
+	echo #will set FILEDIR to Alignment/*.bam'
 	FILEDIR=Alignment/*.bam
 else
 	echo Invalid MODE selected. Please choose either fastq or bam
